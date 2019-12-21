421990e2377a154e257e356eb8c258417c4ad0b1
Andrew Parnham <atparnham@gmail.com>
2019-12-21 12:38:30 +0000

    Using a tuple to define the CSV output

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,7 +1,12 @@
 import fs from "fs";
+import { dateStringToDate } from "./utils";
+import { MatchResult } from "./MatchResult";
+
+//Define a tuple for the result of the csv reading...
+type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 export class CsvFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
 
@@ -13,6 +18,19 @@ export class CsvFileReader {
       .split("\n")
       .map((row: string): string[] => {
         return row.split(",");
-      });
+      })
+      .map(
+        (row: string[]): MatchData => {
+          return [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,
+            row[6]
+          ];
+        }
+      );
   }
 }
