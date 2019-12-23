baeb85ee8fc9abbc84fdd52f1f2d1a4d687aca71
Andrew Parnham <atparnham@gmail.com>
2019-12-23 13:29:29 +0000

    Adding in a static method example.

--- src/Summary.ts
+++ src/Summary.ts
@@ -1,4 +1,6 @@
 import { MatchData } from "./DataReaders/MatchData";
+import { WinsAnalysis } from "./Analyzers/WinsAnalysis";
+import { HtmlReport } from "./ReportTargets/HtmlReport";
 
 export interface Analyzer {
   run(matches: MatchData[]): string;
@@ -9,6 +11,10 @@ export interface OutputTarget {
 }
 
 export class Summary {
+  static winsAnalysisWithHtmlReport(team: string): Summary {
+    return new Summary(new WinsAnalysis(team), new HtmlReport());
+  }
+
   constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
 
   public buildAndPrintReport(matches: MatchData[]): void {
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import { CsvFileMatchReader } from "./DataReaders/CompositionApproach/Readers/CsvFileMatchReader";
 import { MatchReader } from "./DataReaders/CompositionApproach/MatchReader";
-import { ConsoleReport } from "./ReportTargets/ConsoleReport";
-import { WinsAnalysis } from "./Analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
-import { HtmlReport } from "./ReportTargets/HtmlReport";
 
 const matchReader = new MatchReader(new CsvFileMatchReader("football.csv"));
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis("Liverpool"), new ConsoleReport());
+const summary = Summary.winsAnalysisWithHtmlReport("Liverpool");
 summary.buildAndPrintReport(matchReader.matches);
-
-const summaryHtml = new Summary(
-  new WinsAnalysis("Liverpool"),
-  new HtmlReport()
-);
-summaryHtml.buildAndPrintReport(matchReader.matches);
