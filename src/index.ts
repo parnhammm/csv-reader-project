67f59fe09b467ddf6aa4af047706b558e89ede42
Andrew Parnham <atparnham@gmail.com>
2019-12-23 13:23:31 +0000

    Create a basic html report

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,16 @@ import { MatchReader } from "./DataReaders/CompositionApproach/MatchReader";
 import { ConsoleReport } from "./ReportTargets/ConsoleReport";
 import { WinsAnalysis } from "./Analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
+import { HtmlReport } from "./ReportTargets/HtmlReport";
 
 const matchReader = new MatchReader(new CsvFileMatchReader("football.csv"));
 matchReader.load();
 
 const summary = new Summary(new WinsAnalysis("Liverpool"), new ConsoleReport());
 summary.buildAndPrintReport(matchReader.matches);
+
+const summaryHtml = new Summary(
+  new WinsAnalysis("Liverpool"),
+  new HtmlReport()
+);
+summaryHtml.buildAndPrintReport(matchReader.matches);
