af87a1029072de64b7bfd3e3625cfd405f0154f7
Andrew Parnham <atparnham@gmail.com>
2019-12-23 13:17:52 +0000

    Implementing index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { MatchResult } from "./Enums/MatchResult";
 import { CsvFileMatchReader } from "./DataReaders/CompositionApproach/Readers/CsvFileMatchReader";
 import { MatchReader } from "./DataReaders/CompositionApproach/MatchReader";
+import { ConsoleReport } from "./ReportTargets/ConsoleReport";
+import { WinsAnalysis } from "./Analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
 
 const matchReader = new MatchReader(new CsvFileMatchReader("football.csv"));
 matchReader.load();
 
-
-
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary(new WinsAnalysis("Liverpool"), new ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
